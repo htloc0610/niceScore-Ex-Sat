1ca6a8304bc0849689a478851ebb5839e81ca045
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2025-03-10 16:06:09 +0700

    Check

---
+++ dist/config/db.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+require("dotenv/config");
+// Cấu hình kết nối
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, // Tên cơ sở dữ liệu
+process.env.DB_USER, // Tên người dùng
+process.env.DB_PASSWORD, // Mật khẩu
+{
+    // Địa chỉ host (ví dụ: localhost)
+    dialect: process.env.DB_DIALECT, // Loại CSDL (ví dụ: mysql, postgres, sqlite, mssql)
+    logging: false, // Tắt log SQL query trong console (tuỳ chọn)
+    pool: {
+        max: 5, // Số lượng kết nối tối đa
+        min: 0, // Số lượng kết nối tối thiểu
+        acquire: 30000, // Thời gian tối đa (ms) để kết nối
+        idle: 10000, // Thời gian tối đa (ms) kết nối không sử dụng sẽ bị đóng
+    },
+    dialectOptions: {
+        // ssl: {
+        //   require: true,
+        //   rejectUnauthorized: false // Đặt `false` nếu chứng chỉ không được xác thực
+        // }
+        ssl: false,
+    },
+});
+exports.default = sequelize;
+//# sourceMappingURL=db.js.map
