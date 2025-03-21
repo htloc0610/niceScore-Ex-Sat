0202e6ff804bbfa3a9d08cb84158f607848ee1d4
Dien2112 <116195602+Dien2112@users.noreply.github.com>
2025-03-21 18:30:15 +0700

    Fix student table border

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -124,8 +124,13 @@ function RefreshTable(id) {
         <td class="px-4 py-3 text-sm">${student.course.course_name}</td>
         <td class="px-4 py-3 text-sm">${student.program}</td>
         <td class="px-4 py-3 text-sm">${student.permanentAddress.city}</td>
-                <td class="px-4 py-3 text-sm">${student.permanentAddress.city}</td>
-        <td class="px-4 py-3 text-sm" >${student.email}</td>
+        <td class="px-2 py-1 relative group w-[10ch] overflow-hidden whitespace-nowrap text-ellipsis">
+            <span class="block truncate">${student.email}</span>
+            <span class="absolute left-0 hidden group-hover:block bg-white p-2 shadow-lg w-auto max-w-md z-10">
+                ${student.email}
+            </span>
+        </td>f
+        <td class="px-4 py-3 text-sm" >${student.phone_number}</td>
         <td class="px-4 py-3 text-xs">
           <span class="${statusClass}">
             ${student.status.name}
